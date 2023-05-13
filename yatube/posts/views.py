6d095cc69b38f4568fbed39320c87c86dca0ff8d
Oopsybance <mishak784@uandex.ru>
2023-05-13 16:06:17 +0300

    templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -67,17 +67,22 @@ def post_create(request):
 @login_required
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    form = PostForm(request.POST or None, instance=post)
-    context = {
-        'form': form,
-        'is_edit': True,
-        'post': post,
-    }
     if post.author != request.user:
-        return redirect('posts:post_detail', post_id)
+        return redirect('posts:post_detail', post_id=post_id)
+
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post
+    )
     if form.is_valid():
         form.save()
-        return redirect('posts:post_detail', post_id)
+        return redirect('posts:post_detail', post_id=post_id)
+    context = {
+        'post': post,
+        'form': form,
+        'is_edit': True,
+    }
     return render(request, 'posts/create_post.html', context)
 
 
