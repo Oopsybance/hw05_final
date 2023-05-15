bba3411e1f25f4429b3f545a0e524be0c8da7fdc
Oopsybance <mishak784@uandex.ru>
2023-05-16 00:00:13 +0300

    reviy

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
+from django.db.models.fields.files import FileField, ImageFieldFile
 
 from ..models import Post, Group, User, Comment
 
@@ -24,19 +25,6 @@ class PostFormTests(TestCase):
             text='Пост с текстом',
             group=cls.group,
         )
-        cls.small_gif = (
-            b'\x47\x49\x46\x38\x39\x61\x02\x00'
-            b'\x01\x00\x80\x00\x00\x00\x00\x00'
-            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-            b'\x0A\x00\x3B'
-        )
-        cls.uploaded = SimpleUploadedFile(
-            name='small.gif',
-            content=PostFormTests.small_gif,
-            content_type='image/gif'
-        )
 
     def setUp(self):
         self.authorized_client = Client()
@@ -44,10 +32,24 @@ class PostFormTests(TestCase):
 
     def test_create_post(self):
         """Проверка, создает ли форма пост в базе."""
+        small_gif = (
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         posts = list(Post.objects.values_list('id', flat=True))
         form_data = {
             'text': 'Пост с написанным тестом',
             'group': PostFormTests.group.pk,
+            'image': uploaded,
         }
         response = self.authorized_client.post(
             reverse('posts:post_create'),
@@ -64,6 +66,7 @@ class PostFormTests(TestCase):
         self.assertEqual(post_new.text, form_data['text'])
         self.assertEqual(post_new.author, PostFormTests.user)
         self.assertEqual(post_new.group, PostFormTests.group)
+        self.assertEqual(post_new.image, 'posts/' + uploaded.name)
 
     def test_edit_post(self):
         """Проверка, редактируется ли пост."""
@@ -104,30 +107,23 @@ class PostFormTests(TestCase):
         self.assertEqual(self.post.comments.count(), expected_count)
         self.assertEqual(new_comment.text, form_data['text'])
         self.assertEqual(new_comment.author, self.user)
-        self.assertTrue(response.context['comments'])
+        self.assertEqual(form_data['text'], new_comment.text)
 
-    def test_create_iamge(self):
-        """Тест создания поста с картинкой."""
-        posts_count = Post.objects.count()
+    def test_new_comment_guests_client(self):
+        """Гость не может добавлять комментарий."""
+        comments = Comment.objects.count()
         form_data = {
-            'group': PostFormTests.group.id,
-            'text': 'Пост с картинкой',
-            'image': self.uploaded,
+            'text': 'Новый комментарий',
         }
-        response = self.authorized_client.post(
-            reverse('posts:post_create'),
+        self.client.post(
+            reverse('posts:add_comment', kwargs={
+                'post_id': self.post.id
+            }),
             data=form_data,
             follow=True
         )
-        self.assertRedirects(response,
-                             reverse('posts:profile',
-                                     kwargs={'username': self.post.author}))
-        self.assertEqual(Post.objects.count(), posts_count + 1)
-        self.assertTrue(
-            Post.objects.filter(
-                group=PostFormTests.group.id,
-                text='Пост с картинкой',
-                image=Post.objects.get(
-                    text='Пост с картинкой').image,
-            ).exists()
-        )
+        response = self.client.post(
+            reverse('posts:post_detail',
+                    kwargs={'post_id': self.post.id}))
+        self.assertEqual(Comment.objects.count(), comments)
+        self.assertFalse(response.context['comments'])
